e2edcf8cf1b0c090b1eabc7f821254102e8836c1
Kost1117 <44008187+Kost1117@users.noreply.github.com>
2018-11-06 13:32:12 +0300

    Initially prepared code of programm with support of temperatures below 0

--- src/AvTemp.cpp
+++ src/AvTemp.cpp
@@ -7,9 +7,14 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	int K;
+	cin >> K;
+	vector<int> t;
+	
+	
 	return 0;
 }
