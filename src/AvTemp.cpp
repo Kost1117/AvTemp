33015c1e6b800a313cf33eb9634e092b381e43ce
Kost1117 <44008187+Kost1117@users.noreply.github.com>
2018-11-06 13:38:42 +0300

    This is really first commit of full programm

--- src/AvTemp.cpp
+++ src/AvTemp.cpp
@@ -1,20 +1,44 @@
-//============================================================================
-// Name        : AvTemp.cpp
-// Author      : Oleg Kostyuchenko
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int Avr (vector<int>& v)
+{
+	int sum = 0;
+	for (auto i: v)
+	{
+		sum += i;
+	}
+	int av = sum / static_cast<int> (v.size());
+	return av;
+}
+
+void Output (vector<int> v)
+{
+	vector<int> res;
+	for (int i = 0; i < static_cast<int> (v.size()); i++)
+	{
+		if (v[i] > Avr(v))
+		{
+			res.push_back(i);
+		}
+	}
+	cout << res.size() << endl;
+	for (auto i : res)
+	{
+		cout << i << " ";
+	}
+}
 
 int main() {
 	int K;
 	cin >> K;
 	vector<int> t;
-	
-	
+	t.assign(K, 0);
+	for (auto& i : t)
+	{
+		cin >> i;
+	}
+	Output(t);
 	return 0;
 }
